0f59fa49252392ead0b831dd7d9d48e39c5ad2ce
Кирилл <@DESKTOP-SH2U1QQ>
2026-09-12 10:09:27 +0300

    Добавьте файлы проекта.

---
+++ Starostin.KD ZIVT-251 OOP6/Computer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starostin.KD_ZIVT_251_OOP6
+{
+    enum TypeCPU { Настольный, Серверный}
+    enum ManufacturerCPU { Intel, AMD, Apple, Байкал};
+    enum TypeOS { Windows_10, Windows_11, AstraLinux_SE, RedOS, MacOS};
+    interface IComputer
+    {
+        TypeCPU typeCPU { get; }
+        ManufacturerCPU manufacturerCPU { get; }
+        TypeOS typeOS { get; }
+        int clockSpeedCPU { get; }
+        int capacityRAM {  get; }
+        List<string> installedSoftware { get; set; }
+        List<string> usersPC { get; set; }
+    }
+    interface IOverclock
+    {
+        void OverclockTheComputer();
+    }
+    public class Computer: IOverclock, IComputer
+    {
+        TypeCPU typeCPU;
+        ManufacturerCPU manufacturerCPU;
+        TypeOS typeOS;
+        int clockspeedCPU;
+        int capacityRAM;
+        List<string> installedSoftware;
+        List<string> usersPC;
+    }
+}
