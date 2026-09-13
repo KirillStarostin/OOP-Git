d6c8a6da0260192b73a12a6208092df3e87b9b34
Кирилл <star.k2004@mail.ru>
2026-09-13 12:32:59 +0600

    1232

---
+++ Starostin.KD ZIVT-251 OOP6/Manufacturer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Starostin.KD_ZIVT_251_OOP6
+{
+    public enum Country { США, Россия, Китай , ЮжнаяКорея,  Тайвань}
+    interface IManufacturer
+    {
+        string Name { get; set; }
+        Country CountryManufacturer { get; set; }
+        int countEmployees { get; set; }
+    }
+    public class Manufacturer : IManufacturer
+    {
+        public string Name { get; set; }
+        public Country CountryManufacturer { get; set; }
+        public int countEmployees { get; set; }
+
+        public Manufacturer()
+        {
+            Name = "Intel";
+            CountryManufacturer = Country.США;
+            countEmployees = 15067;
+        }
+
+        public Manufacturer(string name, Country country, int count)
+        {
+            Name = name;
+            CountryManufacturer = country;
+            countEmployees = count;
+        }
+
+    }
+}
--- Starostin.KD ZIVT-251 OOP6/Program.cs
+++ Starostin.KD ZIVT-251 OOP6/Program.cs
@@ -53,6 +53,41 @@
             Console.WriteLine($"Сортировка по типу ЦП и производителю");
             foreach (var pc in sortbyCPUandManufacturer)
                 Console.WriteLine($"  {pc.namePC}: {pc.typeCPU} {pc.manufacturerCPU}");
+            Console.WriteLine();
+
+            // SELECT
+            var select = computers.Select(comp => new
+            {
+                name = comp.namePC,
+                ClockSpeed = comp.clockSpeedCPU,
+                RAM = comp.capacityRAM,
+                soft = comp.installedSoftware
+            }).ToList();
+
+            Console.WriteLine($"ПК: Частота-ОЗУ: ПО");
+            foreach (var pc in select)
+                Console.WriteLine($"  {pc.name}: {pc.ClockSpeed} МГц-{pc.RAM} Гб: {string.Join(",", pc.soft)}");
+
+            // Заполняем List<Manufacturer>
+
+            List<Manufacturer> manufacturers = new List<Manufacturer>
+            {
+                new Manufacturer("Intel",   Country.США,        121000),
+                new Manufacturer("AMD",     Country.США,         26000),
+                new Manufacturer("Apple",   Country.США,        164000),
+                new Manufacturer("Байкал",  Country.Россия,        500),
+                new Manufacturer("Huawei",  Country.Китай,      207000),
+                new Manufacturer("Samsung", Country.ЮжнаяКорея, 270000),
+                new Manufacturer("TSMC",    Country.Тайвань,     73000)
+            };
+
+            // Вывод всех производителей
+            Console.WriteLine($"{"Название",-10} | {"Страна",-12} | {"Сотрудники",10}");
+            Console.WriteLine(new string('-', 40));
+            foreach (var m in manufacturers)
+                Console.WriteLine(m);
+
+            Console.WriteLine();
         }
     }
 }
